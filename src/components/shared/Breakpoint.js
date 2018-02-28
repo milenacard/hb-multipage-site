8cbb2c880494476fdcb0b725b23d3df61971bfcd
milenacard <mile.card195@gmail.com>
2018-02-28 15:35:40 -0500

    Creating Breakpoint.js file

---
+++ src/components/shared/Breakpoint.js
@@ -0,0 +1,16 @@
+import './variables.scss'
+
+export default class Breakpoint {
+  static get width () {
+    return (
+    {
+      breakpoint: {
+        small: '639px',
+        mediumMin: '640px',
+        mediumMax: '1023px',
+        large: '1024'
+      }
+    }
+    )
+  }
+}
