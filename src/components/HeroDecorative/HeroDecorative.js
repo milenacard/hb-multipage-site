21950af5193ca488cc984732e23de8798a594f4e
milenacard <mile.card195@gmail.com>
2018-03-01 16:13:28 -0500

    MatchMedia for mediaQuery

--- src/components/HeroDecorative/HeroDecorative.js
+++ src/components/HeroDecorative/HeroDecorative.js
@@ -9,9 +9,12 @@ export class HeroDecorative {
     this.node = this.createNode(data, template)
     this.setDomContainer()
     this.images = {}
+    this.width = {}
+    this.setWidth()
     this.setUrlImages(data)
     this.decorativeContainer = this.node.querySelector('.hero__image-container')
     this.setImages()
+    this.matchMedia()
   }
 
   createNode (data, template) {
@@ -23,6 +26,13 @@ export class HeroDecorative {
     document.body.appendChild(this.node)
   }
 
+  setWidth () {
+    this.width.large = '(min-width:' + breakpoints.width.large + ')'
+    this.width.small = '(min-width:' + breakpoints.width.smallMin + ')'
+    this.width.mediumMin = '(min-width:' + breakpoints.width.mediumMin + ')'
+    this.width.mediumMax = '(min-width:' + breakpoints.width.mediumMax + ')'
+  }
+
   setUrlImages (data) {
     this.images.small = `url('${data.heroDecorative.images.small.url}')`
     this.images.medium = `url('${data.heroDecorative.images.medium.url}')`
@@ -30,13 +40,22 @@ export class HeroDecorative {
   }
 
   setImages () {
-    if (window.innerWidth < breakpoints.mediumMin) {
+    if (window.innerWidth < breakpoints.width.mediumMin) {
       this.decorativeContainer.style.backgroundImage = this.images.small
-      this.decorativeContainer.style.backgroundImage = `no repeat`
-    } else if (window.innerWidth >= breakpoints.mediumMin && window.innerWidth < breakpoints.large) {
+    } else if (window.innerWidth >= breakpoints.width.mediumMin && window.innerWidth < breakpoints.width.large) {
       this.decorativeContainer.style.backgroundImage = this.images.medium
     } else {
       this.decorativeContainer.style.backgroundImage = this.images.large
+    }
+  }
+
+  matchMedia () {
+    if (window.matchMedia(this.width.large).matches) {
+      this.decorativeContainer.style.backgroundImage = this.images.large
+    } else if (window.matchMedia(this.width.mediumMin).matches && window.matchMedia(this.width.mediumMax).matches) {
+      this.decorativeContainer.style.backgroundImage = this.images.medium
+    } else {
+      this.decorativeContainer.style.backgroundImage = this.images.small
     }
   }
 }
