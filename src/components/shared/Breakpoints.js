ca09d56d21ae9edc56b9057a88d3a9507ca30240
milenacard <mile.card195@gmail.com>
2018-03-01 09:36:09 -0500

    Ready breakpoint in the pug

--- src/components/shared/Breakpoints.js
+++ src/components/shared/Breakpoints.js
@@ -1,15 +1,12 @@
-import './variables.scss'
-
 export default class Breakpoint {
   static get width () {
     return (
     {
-      breakpoint: {
-        small: '639px',
-        mediumMin: '640px',
-        mediumMax: '1023px',
-        large: '1024'
-      }
+      smallMin: '320px',
+      smallMax: '639px',
+      mediumMin: '640px',
+      mediumMax: '1023px',
+      large: '1024px'
     }
     )
   }
